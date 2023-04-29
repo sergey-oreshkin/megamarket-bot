6184b0b989cecd9f10f5511ebfb48b03070ad248
iliodor1 <e9991340203@yandex.ru>
2023-04-29 18:39:29 +0500

    add test

---
+++ marketservice/src/test/java/group/megamarket/marketservice/controller/OrderControllerTest.java
@@ -0,0 +1,28 @@
+package group.megamarket.marketservice.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderControllerTest {
+
+    @Test
+    void addProduct() {
+    }
+
+    @Test
+    void getOrder() {
+    }
+
+    @Test
+    void pay() {
+    }
+
+    @Test
+    void deleteProduct() {
+    }
+
+    @Test
+    void clear() {
+    }
+}
