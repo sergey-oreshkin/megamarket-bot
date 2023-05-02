629488ae1797e55c65062de1ce7df666466198cc
Dmitry Soldatov <d.soldatov@astondevs.ru>
2023-05-02 18:07:51 +0500

    docker-compose

---
+++ marketservice/src/main/resources/schema.sql
@@ -0,0 +1,3 @@
+CREATE TABLE IF NOT EXISTS orders (id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,order_date DATE NOT NULL,user_id  BIGINT  NOT NULL,status VARCHAR(40) NOT NULL);
+
+CREATE TABLE IF NOT EXISTS order_product (order_id   BIGINT NOT NULL,product_id BIGINT NOT NULL,quantity   INT    NOT NULL check ( quantity >= 0 ),constraint pk primary key (order_id, product_id),constraint order_id foreign key (order_id) references orders (id));
--- userservice/src/main/java/group/megamarket/userservice/config/ApplicationConfig.java
+++ userservice/src/main/java/group/megamarket/userservice/config/ApplicationConfig.java
@@ -22,7 +22,7 @@ public class ApplicationConfig {
     @Bean
     public StorageService storageService() throws MalformedURLException {
         QName serviceQName = new QName("http://localhost:8000/soap", "StorageServiceImplService");
-        Service service = StorageServiceImplService.create(new URL("http://localhost:8000/soap?wsdl"), serviceQName);
+        Service service = StorageServiceImplService.create(new URL("http://storage:8000/soap?wsdl"), serviceQName);
         return service.getPort(StorageService.class);
     }
 }
